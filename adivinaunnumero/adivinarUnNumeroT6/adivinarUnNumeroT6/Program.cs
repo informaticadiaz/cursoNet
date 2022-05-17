b8393c188bab8a03b6d6daf71928e3b3d37a934c
Ignacio Diaz <informatica.diaz@gmail.com>
2022-05-16 22:28:24 -0300

    Clase 6: Tarea
     Crear un programa para adivinar un numero
    
    1) Generar un número secreto
    aleatorio con la siguiente instruccion:
    
    // Genera un numero entero aleatorio de 1 a 20
    int numeroSecreto = new
    Random(DateTime.Now.Millisecond).Next(1, 21);
    
    2) Pedir al usuario que ingrese un número y que
    intente adivinar el número que eligió la computadora.
    
    3) Si el numero ingresado por el usuario es mayor
    al número secreto, avisarle que es muy grande y que intente de nuevo y que
    vuelva al paso 2.
    
    4) Si el numero ingresado es menor al número
    secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
    
    5) Si el número ingresado coincide con el número
    secreto, sacar el siguiente mensaje:
    
    "Felicitaciones, has adivinado el número
    secreto que era:  [numeroSecreto]"
    "Lo has logrado en [n] intentos!!"
    
    Y finalizar el programa.
    
    Nota:
    Obviamente hay que hacer un bucle y crear un contador de intentos.
    Sean honestos con ustedes mismos y usen sus propias neuronas para resolver el problema, y aunque no lo terminen lo importante es intentarlo.

---
+++ adivinaunnumero/adivinarUnNumeroT6/adivinarUnNumeroT6/Program.cs
@@ -0,0 +1,28 @@
+﻿int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+var n = 0;
+int numero = 0;     
+
+while (numero != numeroSecreto)
+{
+    n++;
+    Console.Write("Ingrese un numero e intente adivinar el numero secreto: ");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero > numeroSecreto)
+    {
+        Console.WriteLine("el número es muy grande, intente de nuevo.");
+    }
+
+    else if (numero < numeroSecreto)
+    {
+        Console.WriteLine("El número es muy chico intente de nuevo.");
+    }
+
+    else if (numero == numeroSecreto)
+    {
+        Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era: " + numeroSecreto);
+        Console.WriteLine("Lo has logrado en " + n + " intentos!!");
+    }
+
+} 
