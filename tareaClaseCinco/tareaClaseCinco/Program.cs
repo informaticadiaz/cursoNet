2ec301cb6bb042762c2f15c18db06691ae79f67d
Ignacio Diaz <informatica.diaz@gmail.com>
2022-05-16 22:38:41 -0300

    Ejecución en consola
    Escribir un programa que haga lo siguiente:
    
    1) Borrar la pantalla.
    2) Pedir el nombre de una persona.
    3) Saludarlo con un texto que diga "¡Hola [NombreIngresado]!"
    4) Preguntar si se quiere continuar.
    5) Si la respuesta es "S" repetir desde el punto 1.
    6) Si la respuesta es "N" finalizar el programa mostrando un mensaje que diga "Programa finalizado correctamente".
    7) Si la respuesta no es ni "S" ni "N" que también finalice el programa, pero mostrando un mensaje que diga "Opción no valida".

---
+++ tareaClaseCinco/tareaClaseCinco/Program.cs
@@ -0,0 +1,25 @@
+﻿var respuesta = "S";
+
+while (respuesta.ToUpper() == "S")
+{
+    // 1) Borra la pantalla
+    Console.Clear();
+    // 2) Pedir el nombre de una persona
+    Console.WriteLine("Hola, Ingrese su nombre:");
+    // Leemos el nombre del usuario
+    string nombre = Console.ReadLine();
+    // 3) Saludarlo con un texto que diga "¡Hola [NombreIngresado]!"
+    Console.Write("Hola " + nombre);
+    Console.WriteLine(". Quiere continuar? (S/N)");
+    respuesta = Console.ReadLine();
+
+    if (respuesta.ToUpper() == "N")
+    {
+        Console.WriteLine("Programa finalizado correctamente");
+    }
+    else
+    {
+        Console.WriteLine("Opcion no valida");
+    }
+
+}
